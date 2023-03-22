19ba33cb9005bb9338ff15988a919f807f9fd32a
Miguel Muscat <miguelmuscat93@gmail.com>
2023-03-22 09:57:47 +0100

    Add plugin tests

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use RebelCode\WpSdk\Module;
 use RebelCode\WpSdk\Plugin;
 use RebelCode\WpSdk\Tests\Helpers\BrainMonkeyTest;
 use function Brain\Monkey\Functions\expect;
@@ -34,5 +36,43 @@ class PluginTest extends TestCase
 
         $plugin = Plugin::create(__DIR__ . '/Stubs/plugin.php', '.');
         $this->assertEquals('baz', $plugin->get('foo.bar'));
+    }
+
+    public function testProxiesGetToContainer()
+    {
+        $c = $this->createMock(ContainerInterface::class);
+        $plugin = new Plugin(__DIR__, $c, []);
+
+        $c->expects($this->once())->method('get')->with('foo')->willReturn('bar');
+
+        $this->assertEquals('bar', $plugin->get('foo'));
+    }
+
+    public function testProxiesHasToContainer()
+    {
+        $c = $this->createMock(ContainerInterface::class);
+        $plugin = new Plugin(__DIR__, $c, []);
+
+        $c->expects($this->once())->method('has')->with('foo')->willReturn(true);
+
+        $this->assertTrue($plugin->has('foo'));
+    }
+
+    public function testRunsModules()
+    {
+        $c = $this->createMock(ContainerInterface::class);
+        $modules = [
+            $this->createMock(Module::class),
+            $this->createMock(Module::class),
+            $this->createMock(Module::class),
+        ];
+
+        $plugin = new Plugin(__DIR__, $c, $modules);
+
+        foreach ($modules as $module) {
+            $module->expects($this->once())->method('run')->with($c, $plugin);
+        }
+
+        $plugin->run();
     }
 }
