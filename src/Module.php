543000b87c9a0bafdeeb6b6226c0f90cc2a5d69b
Miguel Muscat <miguelmuscat93@gmail.com>
2023-11-30 10:24:20 +0100

    feat: add compatibility with iterable modules

--- src/Module.php
+++ src/Module.php
@@ -4,10 +4,33 @@ namespace RebelCode\WpSdk;
 
 use Dhii\Services\Extension;
 use Dhii\Services\Service;
+use IteratorAggregate;
 use Psr\Container\ContainerInterface;
+use Traversable;
 
-class Module
+/** @implements IteratorAggregate<string,callable> */
+class Module implements IteratorAggregate
 {
+    /** For compatibility with iterable modules. */
+    public function getIterator(): Traversable
+    {
+        yield from $this->getFactories();
+        yield from $this->getExtensions();
+
+        return function (ContainerInterface $c) {
+            foreach ($this->getHooks() as $key => $handlers) {
+                /** @var $handlers Handler[] */
+                $handlers = (array) $handlers;
+
+                foreach ($handlers as $handler) {
+                    $handler->attach($key, $c);
+                }
+            }
+
+            $this->run($c);
+        };
+    }
+
     /** Runs the module */
     public function run(ContainerInterface $c, Plugin $plugin): void
     {
